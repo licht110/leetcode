b49864a69bcb274e3f0a94c7e607d241e87ffaab
itorihito <itorihito@yahoo.co.jp>
2020-11-23 17:18:45 +0900

    improve performance 15

--- algorithms/15/main.go
+++ algorithms/15/main.go
@@ -1,52 +1,42 @@
 func threeSum(nums []int) [][]int {
-    m := map[int][]int{}
-    ans := map[int]map[int]int{}
-    for i, n := range nums {
-        if indices, ok := m[n] ; ok {
-            m[n] = append(indices, i)
-        } else {
-            m[n] = []int{i}
-        }
-    }
+    sort.Slice(nums, func(i,j int) bool {return nums[i] < nums[j]})
+    m := map[int]int{}
+    buf := map[int]map[int]int{}
     for i := 0 ; i < len(nums)-1 ; i++ {
         for j := i+1 ; j < len(nums) ; j++ {
             target := -(nums[i]+nums[j])
-            if indices, ok := m[target] ; ok {
-                if !duplicate(indices, i, j) {
-                    ans = add(ans, []int{nums[i], nums[j], target})
-                }
+            if _, ok := m[target]; ok {
+                buf = add(buf, []int{nums[i], nums[j], target})
             }
         }
-    }
-    ret := [][]int{}
-    for k1, v2 := range ans {
-        for k2, v2 := range v2 {
-            ret = append(ret, []int{k1, k2, v2})
+        if _, ok := m[nums[i]]; !ok {
+            m[nums[i]] = i
         }
     }
-    return ret
+    return convert(buf)
 }
 
-func duplicate(indices []int, i,j int) bool {
-    for _, index := range indices {
-        if index != i && index != j {
-            return false
-        }
-    }
-    return true
-}
-
-func add(ans map[int]map[int]int, set []int) map[int]map[int]int {
-    sort.Slice(set, func(i,j int) bool {return set[i] < set[j]})
-    i, j, k := set[0], set[1], set[2]
-    if m1, ok := ans[i] ; ok {
-        if _, ok := m1[j] ; !ok {
-            ans[i][j] = k
-        }
-    } else {
-        ans[i] = map[int]int{
-            j: k,
+func convert(buf map[int]map[int]int) [][]int {
+    ans := [][]int{}
+    for n1, m1 := range buf {
+        for n2, n3 := range m1 {
+            ans = append(ans, []int{n1,n2,n3})
         }
     }
     return ans
+}
+
+func add(buf map[int]map[int]int, set []int) map[int]map[int]int {
+    sort.Slice(set, func(i,j int) bool {return set[i] < set[j]})
+    n1, n2, n3 := set[0], set[1], set[2]
+    if m1, ok := buf[n1]; ok {
+        if _, ok := m1[n2]; !ok {
+            buf[n1][n2] = n3
+        }
+    } else {
+        buf[n1] = map[int]int{
+            n2: n3,
+        }
+    }
+    return buf
 }
