c1be1eea77ad126fcf7cfa5ef7cc2387ee386702
itorihito <itorihito@yahoo.co.jp>
2020-11-25 23:35:58 +0900

    add 700

---
+++ algorithms/700/main.go
@@ -0,0 +1,11 @@
+func searchBST(root *TreeNode, val int) *TreeNode {
+    if root == nil {
+        return nil
+    } else if root.Val == val {
+        return root
+    } else if root.Val > val {
+        return searchBST(root.Left, val)
+    } else {
+        return searchBST(root.Right, val)
+    }
+}
