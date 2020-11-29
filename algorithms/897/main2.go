206c7168209caf54d7e501994c55e92d5096d48e
itorihito <itorihito@yahoo.co.jp>
2020-11-29 17:00:05 +0900

    refactor 897 another solution

--- algorithms/897/main2.go
+++ algorithms/897/main2.go
@@ -4,19 +4,10 @@ func increasingBST(root *TreeNode) *TreeNode {
 
 func recursive(curr *TreeNode, root *TreeNode) *TreeNode {
     if curr == nil {
-        return nil
+        return root
     }
-    var smallest *TreeNode
-    if curr.Left != nil {
-        smallest = recursive(curr.Left, curr)
-    } else {
-        smallest = curr
-    }
-    if curr.Right != nil {
-        curr.Right = recursive(curr.Right, root)
-    } else {
-        curr.Right = root
-    }
+    smallest := recursive(curr.Left, curr)
     curr.Left = nil
+    curr.Right = recursive(curr.Right, root)
     return smallest
 }
